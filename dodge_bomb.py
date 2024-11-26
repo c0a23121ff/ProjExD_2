a47191ff6594817f1025b3c59ad3423eaca342b1
Hayashi Haruto <c0a23121ff@edu.teu.ac.jp>
2024-11-26 14:07:38 +0900

    練習問題1:DELTA

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,6 +4,11 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1100, 650
+
+#押下キーと移動量の対応関係を表す辞書DELTAを定義する
+DELTA = {pg.K_UP: (0, -5), pg.K_DOWN: (0, 5),
+        pg.K_LEFT: (-5, 0), pg.K_RIGHT: (5, 0),}
+
 os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 
@@ -16,6 +21,10 @@ def main():
     kk_rct.center = 900, 400
     clock = pg.time.Clock()
     tmr = 0
+
+    
+    
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -24,14 +33,18 @@ def main():
 
         key_lst = pg.key.get_pressed()
         sum_mv = [0, 0]
-        if key_lst[pg.K_UP]:
-            sum_mv[1] -= 5
-        if key_lst[pg.K_DOWN]:
-            sum_mv[1] += 5
-        if key_lst[pg.K_LEFT]:
-            sum_mv[0] -= 5
-        if key_lst[pg.K_RIGHT]:
-            sum_mv[0] += 5
+        for key, tpl in DELTA.items():
+            if key_lst[key]:
+                sum_mv[0] += tpl[0]
+                sum_mv[1] += tpl[1]
+        # if key_lst[pg.K_UP]:
+        #     sum_mv = DELTA[pg.K_UP]
+        # if key_lst[pg.K_DOWN]:
+        #     sum_mv = DELTA[pg.K_DOWN]
+        # if key_lst[pg.K_LEFT]:
+        #     sum_mv = DELTA[pg.K_LEFT]
+        # if key_lst[pg.K_RIGHT]:
+        #     sum_mv = DELTA[pg.K_RIGHT]
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
         pg.display.update()
